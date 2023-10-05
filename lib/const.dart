2e9b9afc642b10b79d8ece5fbc45959865351159
fazilAravind <aravindfazilfazil@gmail.com>
2023-10-05 19:38:18 +0530

    Aravid - login page ui completed

---
+++ lib/const.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color colorPrimary = Color(0xFFFFCE33);
+  static const Color colorPrimaryDark = Color(0xFFFFCE33);
+  static const Color colorAccent = Color(0xFFFFCE33);
+  static const Color colorWhite = Color(0xFFFFFFFF);
+  static const Color colorGrey = Color(0xFFCECECE);
+  static const Color colorGrey1 = Color(0xFF888888);
+  static const Color colorGrey2 = Color(0xFF5A5A5A);
+  static const Color colorBlack = Color(0xFF1D2021);
+  static const Color colorRed = Color(0xFFD71C32);
+  static const Color colorTransparent = Color(0xFF7FFBFAFA);
+  static const Color notificationTitle = Color(0xFF000000);
+  static const Color notificationSubTitle = Color(0xFF838383);
+  static const Color golden = Color(0xFFFFD900);
+  static const Color progressDrawableBg = Color(0xFFD1D1D1);
+  static const Color toolbar = Color(0xFF23BE59);
+}
