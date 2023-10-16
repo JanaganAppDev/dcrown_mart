259c9d28e4782d39a89faceae42f6abe2f9bc3bb
KarkiKathir <kathiresh.karki@gmail.com>
2023-10-16 09:43:11 +0530

    Kathiresh- Otp Page API integration

--- lib/service/api_response.dart
+++ lib/service/api_response.dart
@@ -1,3 +1,4 @@
 String base_url = "http://localhost:5000/api";
 String forgot = "$base_url/forgots/forgot";
 String otp = "$base_url/forgots/otpVerify/2";
+String newpassword = "$base_url/users/update";
