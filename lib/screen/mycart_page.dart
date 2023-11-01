7c02eae33a2a6b2476dc72249bb5b12bd723d225
KarkiKathir <kathiresh.karki@gmail.com>
2023-11-01 18:02:53 +0530

    Kathiresh- feedback page functions and My cart Page UI

--- lib/screen/mycart_page.dart
+++ lib/screen/mycart_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import 'home_page.dart';
 
 class MyCartPage extends StatefulWidget {
@@ -30,122 +29,167 @@ class _MyCartPageState extends State<MyCartPage> {
           },
         ),
       ),
-      body: SingleChildScrollView(
-        padding: EdgeInsets.all(8.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Container(
-              margin: EdgeInsets.all(8),
-              height: 100.0,
-              width: screenWidth,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(10),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey,
-                    blurRadius: 4,
-                    spreadRadius: 0,
-                    offset: Offset(0, 0),
-                  ),
-                ],
-              ),
-              child: Row(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Expanded(
+            child: SingleChildScrollView(
+              padding: EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  Image.asset("assets/dishwash.jpeg",
-                      width: 50.0, height: 50.0),
-                  SizedBox(width: 10.0),
-                  Column(
-                    children: [
-                      SizedBox(height: 10.0),
-                      Text(
-                        "DISHWASH LIQUID",
-                        style: TextStyle(
-                          fontSize: 16.0,
-                          color: Colors.grey[700],
+                  // Your existing cart item here
+                  Container(
+                    margin: EdgeInsets.all(8),
+                    height: 100.0,
+                    width: screenWidth,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey,
+                          blurRadius: 4,
+                          spreadRadius: 0,
+                          offset: Offset(0, 0),
                         ),
-                      ),
-                      SizedBox(height: 10.0),
-                      Text(
-                        "Rs.150",
-                        style: TextStyle(
-                          color: Colors.yellow[500],
-                        ),
-                      ),
-                      SizedBox(height: 5.0),
-                      Container(
-                        height: 25.0,
-                        width: 70.0,
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(20.0),
-                          border: Border.all(
-                            color: Colors.yellow,
-                          ),
-                        ),
-                        child: Center(
-                          child: Text(
-                            "500ML",
-                            style: TextStyle(
-                              fontSize: 13.0,
-                              color: Colors.grey[700],
+                      ],
+                    ),
+                    child: Row(
+                      children: [
+                        Image.asset("assets/dishwash.jpeg",
+                            width: 50.0, height: 50.0),
+                        SizedBox(width: 10.0),
+                        Column(
+                          children: [
+                            SizedBox(height: 10.0),
+                            Text(
+                              "DISHWASH LIQUID",
+                              style: TextStyle(
+                                fontSize: 16.0,
+                                color: Colors.grey[700],
+                              ),
                             ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                  SizedBox(width: 50.0),
-                  Column(
-                    children: [
-                      SizedBox(height: 10.0),
-                      IconButton(
-                        icon: Icon(Icons.delete_forever),
-                        onPressed: () {},
-                      ),
-                      SizedBox(height: 10.0),
-                      InkWell(
-                        onTap: () {
-                          setState(() {
-                            if (itemCount > 0) {
-                              itemCount--;
-                            }
-                          });
-                        },
-                        child: Container(
-                          height: 25.0,
-                          width: 70.0,
-                          decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(20.0),
-                            border: Border.all(
-                              color: Colors.yellow,
+                            SizedBox(height: 10.0),
+                            Text(
+                              "Rs.150",
+                              style: TextStyle(
+                                color: Colors.yellow[500],
+                              ),
                             ),
-                          ),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Icon(Icons.remove, color: Colors.yellow),
-                              Text(
-                                itemCount.toString(),
-                                style: TextStyle(
-                                  fontSize: 13.0,
-                                  color: Colors.grey[700],
+                            SizedBox(height: 5.0),
+                            Container(
+                              height: 25.0,
+                              width: 70.0,
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(20.0),
+                                border: Border.all(
+                                  color: Colors.yellow,
                                 ),
                               ),
-                              Icon(Icons.add, color: Colors.yellow),
-                            ],
-                          ),
+                              child: Center(
+                                child: Text(
+                                  "500ML",
+                                  style: TextStyle(
+                                    fontSize: 13.0,
+                                    color: Colors.grey[700],
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
                         ),
-                      ),
-                    ],
+                        SizedBox(width: 50.0),
+                        Column(
+                          children: [
+                            SizedBox(height: 10.0),
+                            IconButton(
+                              icon: Icon(Icons.delete_forever),
+                              onPressed: () {},
+                            ),
+                            SizedBox(height: 10.0),
+                            InkWell(
+                              onTap: () {
+                                setState(() {
+                                  if (itemCount > 0) {
+                                    itemCount--;
+                                  }
+                                });
+                              },
+                              child: Container(
+                                height: 25.0,
+                                width: 70.0,
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.circular(20.0),
+                                  border: Border.all(
+                                    color: Colors.yellow,
+                                  ),
+                                ),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Icon(Icons.remove, color: Colors.yellow),
+                                    Text(
+                                      itemCount.toString(),
+                                      style: TextStyle(
+                                        fontSize: 13.0,
+                                        color: Colors.grey[700],
+                                      ),
+                                    ),
+                                    Icon(Icons.add, color: Colors.yellow),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
                 ],
               ),
-            )
-          ],
-        ),
+            ),
+          ),
+          // New container at the bottom
+          Container(
+            color: Colors.yellow[600],
+            height: 60.0,
+            child: Center(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "1 Item | Rs150",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 16.0,
+                    ),
+                  ),
+                  SizedBox(width: 100.0),
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.white,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30.0),
+                      ),
+                    ),
+                    child: Text(
+                      "Order   >",
+                      style: TextStyle(
+                        color: Colors.yellow,
+                        fontSize: 16.0,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
